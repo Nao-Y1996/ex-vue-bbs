72e272e7ff07ab5cba6ea45fbbfbbb91ac38b46f
nao.yamada <nao.yamada@B0363-MACB.local>
2022-07-13 14:03:03 +0900

    initial commit

---
+++ src/views/bbs.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>vue掲示板</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class Bbs extends Vue {}
+</script>
+
+<style scoped></style>
