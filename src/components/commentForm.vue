c10dfaa6abc3f0019ed9f5b598810eaf53703670
nao.yamada <nao.yamada@B0363-MACB.local>
2022-07-13 17:16:57 +0900

    コメント投稿の入力値チェックを実装

--- src/components/commentForm.vue
+++ src/components/commentForm.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
     <form action="">
+      <span class="error" v-if="nameError != ''">{{ nameError }}<br /></span>
       <label for="commenterName">コメント者名</label>
-      <input
-        type="text"
-        name=""
-        id="commenterName"
-        v-model="commenterName"
-      /><br />
+      <input type="text" id="commenterName" v-model="commenterName" /><br />
+      <span class="error" v-if="contentError != ''"
+        >{{ contentError }}<br
+      /></span>
       <label for="commentContent">コメント内容</label><br />
       <textarea
-        name=""
-        id=""
+        id="commentContent"
         cols="30"
         rows="10"
         v-model="commentContent"
@@ -27,7 +25,7 @@
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
-export default class CommentForm extends Vue {
+export default class CopmCommentForm extends Vue {
   // 記事ID
   @Prop()
   private articleId!: number;
@@ -35,20 +33,58 @@ export default class CommentForm extends Vue {
   private commenterName = "";
   // コメント内容
   private commentContent = "";
+  // 名前の未入力のエラーメッセージ
+  private nameError = "";
+  // コメント内容の未入力のエラーメッセージ
+  private contentError = "";
 
   /**
    * コメントを投稿する.
    */
   postComment(articleId: number) {
-    this.$store.commit("createComment", {
-      articleId: articleId,
-      commenterName: this.commenterName,
-      commentContent: this.commentContent,
-    });
-    this.commenterName = "";
-    this.commentContent = "";
+    if (this.inputHasError()) {
+      return;
+    } else {
+      this.$store.commit("createComment", {
+        articleId: articleId,
+        commenterName: this.commenterName,
+        commentContent: this.commentContent,
+      });
+      this.commenterName = "";
+      this.commentContent = "";
+    }
+  }
+  /**
+   * コメント投稿の際に名前と投稿内容のバリデーションチェックをする.
+   *
+   * @return バリデーションをクリアしたかどうかの真偽値
+   */
+  inputHasError(): boolean {
+    // 名前の入力チェック
+    if (this.commenterName === "") {
+      this.nameError = "名前を入力してください";
+    } else if (this.commenterName.length > 50) {
+      this.nameError = "名前は50文字以内で入力してください";
+    } else {
+      this.nameError = "";
+    }
+    // 投稿内容の入力チェック
+    if (this.commentContent === "") {
+      this.contentError = "コメント内容を入力してください";
+    } else {
+      this.contentError = "";
+    }
+    if (this.nameError !== "" || this.contentError !== "") {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.error {
+  color: red;
+}
+</style>
